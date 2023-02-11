8be990277bec8d3b4c9bf8bac0b7ed4553403efe
Bruno Ribeiro <brf147@gmail.com>
2023-02-10 23:34:06 -0400

    add first page using live_view

---
+++ lib/food_elxpro_web/live/main/main_live.ex
@@ -0,0 +1,7 @@
+defmodule FoodElxproWeb.MainLive do
+  use FoodElxproWeb, :live_view
+
+  def mount(_params, _session, socket) do
+    {:ok, socket}
+  end
+end
--- lib/food_elxpro_web/router.ex
+++ lib/food_elxpro_web/router.ex
@@ -17,7 +17,7 @@ defmodule FoodElxproWeb.Router do
   scope "/", FoodElxproWeb do
     pipe_through :browser
 
-    get "/", PageController, :index
+    live "/", MainLive, :index
   end
 
   # Other scopes may use custom stacks.
