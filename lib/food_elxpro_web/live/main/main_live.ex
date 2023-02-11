75a52f6807b021b6806b629943482c25ac5978aa
Bruno Ribeiro <brf147@gmail.com>
2023-02-11 15:08:22 -0400

    breaking main page with smalls components

--- lib/food_elxpro_web/live/main/main_live.ex
+++ lib/food_elxpro_web/live/main/main_live.ex
@@ -1,5 +1,7 @@
 defmodule FoodElxproWeb.MainLive do
   use FoodElxproWeb, :live_view
+  alias FoodElxproWeb.Main.Hero
+  alias FoodElxproWeb.Main.Items
 
   def mount(_params, _session, socket) do
     {:ok, socket}
